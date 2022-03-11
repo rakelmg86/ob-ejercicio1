1f200b666ca4cce22684aaa4a88ec22b929d2913
Raquel Menendez Gonzalez <rakel8686@gmail.com>
2022-03-11 10:27:38 +0100

    leccion 3 ejercicio1

---
+++ L3ejercicio1.py
@@ -0,0 +1,11 @@
+nombre = "Raquel"
+apellidos = "Menendez Gonzalez"
+edad = 35
+telefono = "931664001"
+casado = True
+con_hijos = True
+lista_de_amigos = ('anna', 'leticia', 'sonia', 'cristina', 'sara')
+peliculas_vistas = {'pelicula1': 'a traves de mi ventana', 'pelicula2': 'encanto', 'pelicula3': 'sin salida'}
+
+print(nombre, apellidos, edad, telefono, casado, con_hijos, lista_de_amigos,peliculas_vistas)
+
