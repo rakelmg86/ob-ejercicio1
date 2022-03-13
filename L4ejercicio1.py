21761f0d723a032080323ffeef59788d4ca80a9c
rakelmg86 <100537613+rakelmg86@users.noreply.github.com>
2022-03-13 19:21:06 +0100

    Add files via upload

---
+++ L4ejercicio1.py
@@ -0,0 +1,7 @@
+print("**Programa para saber si eres mayor de edad**\n")
+
+edad = int(input("Introduce tu edad: "))
+if edad >= 18:
+           print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
