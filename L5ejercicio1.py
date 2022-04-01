859e400b339883be7174e69226ef34c03e78626e
rakelmg86 <100537613+rakelmg86@users.noreply.github.com>
2022-04-01 11:09:19 +0200

    Add files via upload

---
+++ L5ejercicio1.py
@@ -0,0 +1,10 @@
+def area_triangulo(altura, base):
+    return (altura * base)/2
+
+print (area_triangulo(5,8))
+
+
+def area_circulo(radio)
+      return 3.141515 *(radio**2)
+    
+print(area_circulo(5)
